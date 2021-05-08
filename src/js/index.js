7e5d7a571cf5111a2b04495754781488089bde7a
Jora Xazaryan <jora.xazaryan@gmail.com>
2021-05-08 14:14:23 +0400

    create first screen

---
+++ src/js/index.js
@@ -0,0 +1,21 @@
+// import Vue from 'vue';
+
+const $ = require("jquery");
+const jQuery = $;
+window.jQuery = window.$ = jQuery;
+
+// require('owl.carousel');
+// require('../js/popper.min.js');
+// require('../js/bootstrap.min.js');
+// require('@fancyapps/fancybox');
+// require('lightgallery');
+// require('bootstrap');
+// import { Alert, Button, Carousel, Collapse, Dropdown, Tab, Modal, Popover, ScrollSpy, Toast, Tooltip } from 'bootstrap';
+// import { Modal } from 'bootstrap';
+// import Bootstrap from 'Bootstrap';
+// console.log(Bootstrap);
+// console.log(Tooltip);
+
+$(document).ready(function () {
+
+});
